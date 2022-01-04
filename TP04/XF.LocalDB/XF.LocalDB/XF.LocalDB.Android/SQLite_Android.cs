68b1ecd50d3088a9cc077cdb995542bee44d7ebe
Stefany Lisboa <stefany22lisboa@gmail.com>
2022-01-04 19:58:35 -0300

    Upload

---
+++ TP04/XF.LocalDB/XF.LocalDB/XF.LocalDB.Android/SQLite_Android.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using XF.LocalDB.Data;
+
+namespace XF.LocalDB.Droid
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLite_Android()
+        {
+        }
+        public SQLite.SQLiteConnection
+        GetConexao()
+        {
+            var arquivodb = "ifspdb.db3";
+            string caminho = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var local = Path.Combine(caminho, arquivodb);
+            var conexao = new
+            SQLite.SQLiteConnection(local);
+            return conexao;
+        }
+    }
+}
---
+++ TP04/XF.LocalDB/XF.LocalDB/XF.LocalDB/Data/ISQLite.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.LocalDB.Data
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConexao();
+
+    }
+}
